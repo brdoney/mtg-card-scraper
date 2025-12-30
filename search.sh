cbe1a96d9970e0f840670a3c258d2429f13ff3ae
Brendan Doney <bre.doney@gmail.com>
2025-12-29 23:19:52 -0500

    Show output for search with no results

--- search.sh
+++ search.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 file=$(python3 -u search.py "$1" | tee /dev/tty | tail -1)
-./search-lib.sh --title="Search: $1" "$file"
+
+is_empty=$(jq -e length "$file")
+if [[ is_empty -ne 0 ]]; then
+	./search-lib.sh --title="Search: $1" "$file"
+else
+	echo "No results found"
+fi
